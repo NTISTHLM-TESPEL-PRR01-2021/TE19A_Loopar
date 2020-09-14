d94e089bdb68edddae54a36b7cbf546760513770
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-14 09:09:17 +0200

    Initial commit

---
+++ Loopar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Loopar
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      
+      int y = 40;
+
+      string name = "";
+      string password = "";
+
+      //  || (name == "herbert" && password == "123")
+
+      while (!(name == "micke" && password == "sallad") && !(name == "herbert" && password == "123"))
+      {
+        System.Console.WriteLine("Skriv in ditt namn: ");
+        name = Console.ReadLine();
+
+        System.Console.WriteLine("Skriv in ditt lösenord: ");
+        password = Console.ReadLine();
+
+        // System.Console.WriteLine(y);
+        // y--;
+      }
+
+      System.Console.WriteLine("VÄLKOMMEN");
+
+      Console.ReadLine();
+    }
+  }
+}
